412221018d3375545fdabef174ee7b9ff3ba89f0
Lim Kimnguon <limkimnguon2020@gmail.com>
2022-03-12 19:14:52 +0700

    TP08 - (Authentication frontend & APIs)

---
+++ Ex1_VueJS_Form/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text">Welcome! This is Home Page</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+  .text {
+    font-size: 20px;
+    text-align: center;
+    margin-top: 250px;
+  }
+</style>
---
+++ Ex3_Form with API/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="box">
+    <p>You're in Home Page</p>
+    <p>Well done</p>
+    <button class="btn" @click="goToLogin">Back to login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToLogin() {
+      this.$router.push("/login");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.box {
+  text-align: center;
+}
+.btn {
+  background-color: rgb(71, 232, 243);
+  padding: 10px;
+  border-radius: 20px;
+  border: 1px solid black;
+}
+</style>
